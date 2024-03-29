de682fec2b5dd7d7874afe919585c9c8e441d59e
YEUNG KIEN XIN <kienxin90@gmail.com>
2024-03-29 23:03:40 +0800

    change Form1 to FormHome

--- Restaurant_Food_Ordering_System/My Project/Application.Designer.vb
+++ Restaurant_Food_Ordering_System/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Restaurant_Food_Ordering_System.Form1
+            Me.MainForm = Global.Restaurant_Food_Ordering_System.FormHome
         End Sub
     End Class
 End Namespace
