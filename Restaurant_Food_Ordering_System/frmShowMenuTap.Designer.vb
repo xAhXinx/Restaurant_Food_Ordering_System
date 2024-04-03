32bdfc38b10b9d825b10ef8ff7e514c0f61381bd
pallatanpt <pallatanpt@gmail.com>
2024-04-03 09:14:04 +0800

    Added TapOrderForm + Database
    Added TapOrderForm + Database

---
+++ Restaurant_Food_Ordering_System/frmShowMenuTap.Designer.vb
@@ -0,0 +1,51 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmShowMenuTap
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.pnlOutput = New System.Windows.Forms.Panel()
+        Me.SuspendLayout()
+        '
+        'pnlOutput
+        '
+        Me.pnlOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.pnlOutput.Location = New System.Drawing.Point(201, 12)
+        Me.pnlOutput.Name = "pnlOutput"
+        Me.pnlOutput.Size = New System.Drawing.Size(697, 419)
+        Me.pnlOutput.TabIndex = 0
+        '
+        'FrmShowMenuTap
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(910, 517)
+        Me.Controls.Add(Me.pnlOutput)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "FrmShowMenuTap"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "frmShowMenu"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents pnlOutput As Panel
+End Class
---
+++ Restaurant_Food_Ordering_System/frmShowMenuTap.vb
@@ -0,0 +1,51 @@
+﻿Imports System.Data.SqlClient
+Public Class FrmShowMenuTap
+
+    Dim intTableNum As Integer
+    Const INT_MENU_HEIGHT As Integer = 4
+    Const INT_MENU_WIDTH As Integer = 4
+    Dim btnArr As Button() = {}
+    Dim intPageNUM As Integer = 1
+
+
+    Private Sub FrmShowMenuTap_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim menuItemsResult As MenuItem() = ReturnMenuItems("BEV")
+
+        'Init buttons if not exist
+        If btnArr.Length = 0 Then
+            For i = 1 To INT_MENU_HEIGHT
+                For j = 1 To INT_MENU_WIDTH
+                    Dim btnNew As New System.Windows.Forms.Button()
+                    Dim menuSize As Size = pnlOutput.Size
+
+                    btnArr.Append(btnNew)
+                    pnlOutput.Controls.Add(btnNew)
+                    btnNew.Height = (menuSize.Height - 10D) / (INT_MENU_HEIGHT)
+                    btnNew.Width = (menuSize.Width - 10D) / (INT_MENU_WIDTH)
+                    btnNew.Top = menuSize.Height * ((i - 1) / INT_MENU_HEIGHT)
+                    btnNew.Left = menuSize.Width * ((j - 1) / INT_MENU_WIDTH)
+                    btnNew.Text = "Press me"
+
+
+                Next
+            Next
+        End If
+
+    End Sub
+
+    Private Function ReturnMenuItems(menuType As String) As MenuItem()
+        Dim db As New OrderDataContext()
+
+        Dim menuItemsOutput As MenuItem() = {}
+
+        For Each row In db.MenuItems
+            If Not row.MenuItemID.IndexOf(menuType) = -1 Then
+                menuItemsOutput.Append(row)
+            End If
+        Next
+        Return menuItemsOutput
+    End Function
+
+End Class
+
+
