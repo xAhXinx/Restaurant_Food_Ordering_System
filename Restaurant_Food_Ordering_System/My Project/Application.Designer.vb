31e780b3972aeb8573e341df5c5be55e7a6070d2
YEUNG KIEN XIN <kienxin90@gmail.com>
2024-04-14 17:27:27 +0800

    Change name from "Form" to "Frm" and comment all database related code

--- Restaurant_Food_Ordering_System/My Project/Application.Designer.vb
+++ Restaurant_Food_Ordering_System/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Restaurant_Food_Ordering_System.FrmShowMenuTap
+            Me.MainForm = Global.Restaurant_Food_Ordering_System.FormHome
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
--- Restaurant_Food_Ordering_System/frmShowMenuTap.vb
+++ Restaurant_Food_Ordering_System/frmShowMenuTap.vb
@@ -9,27 +9,27 @@ Public Class FrmShowMenuTap
 
 
     Private Sub FrmShowMenuTap_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim menuItemsResult As MenuItem() = ReturnMenuItems("BEV")
+        'Dim menuItemsResult As MenuItem() = ReturnMenuItems("BEV")
 
-        'Init buttons if not exist
-        If btnArr.Length = 0 Then
-            For i = 1 To INT_MENU_HEIGHT
-                For j = 1 To INT_MENU_WIDTH
-                    Dim btnNew As New System.Windows.Forms.Button()
-                    Dim menuSize As Size = pnlOutput.Size
+        ''Init buttons if not exist
+        'If btnArr.Length = 0 Then
+        '    For i = 1 To INT_MENU_HEIGHT
+        '        For j = 1 To INT_MENU_WIDTH
+        '            Dim btnNew As New System.Windows.Forms.Button()
+        '            Dim menuSize As Size = pnlOutput.Size
 
-                    btnArr.Append(btnNew)
-                    pnlOutput.Controls.Add(btnNew)
-                    btnNew.Height = (menuSize.Height - 10D) / (INT_MENU_HEIGHT)
-                    btnNew.Width = (menuSize.Width - 10D) / (INT_MENU_WIDTH)
-                    btnNew.Top = menuSize.Height * ((i - 1) / INT_MENU_HEIGHT)
-                    btnNew.Left = menuSize.Width * ((j - 1) / INT_MENU_WIDTH)
-                    btnNew.Text = "Press me"
+        '            btnArr.Append(btnNew)
+        '            pnlOutput.Controls.Add(btnNew)
+        '            btnNew.Height = (menuSize.Height - 10D) / (INT_MENU_HEIGHT)
+        '            btnNew.Width = (menuSize.Width - 10D) / (INT_MENU_WIDTH)
+        '            btnNew.Top = menuSize.Height * ((i - 1) / INT_MENU_HEIGHT)
+        '            btnNew.Left = menuSize.Width * ((j - 1) / INT_MENU_WIDTH)
+        '            btnNew.Text = "Press me"
 
 
-                Next
-            Next
-        End If
+        '        Next
+        '    Next
+        'End If
 
     End Sub
 
